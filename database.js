f6e0c6c3b42c70b82dc3eef7e774dff89e70bf88
AlanWalkup <101841213+AlanWalkup@users.noreply.github.com>
2022-03-23 15:48:11 -0600

    Update database.js

--- database.js
+++ database.js
@@ -1,9 +1,9 @@
 let config = {
-	host: "localhost",
-	user: "root",
-	password: "",
+	host: "us-cdbr-east-05.cleardb.net",
+	user: "b852c535a5b2ba",
+	password: "abeb3713",
 	port: 3306,
-	database: "adoption_system",
+	database: "heroku_a682f8d2e60d510",
 	secret_key: "AlanWR11092002"
 };
 exports.config = config;
